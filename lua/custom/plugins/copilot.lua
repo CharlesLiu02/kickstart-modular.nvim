2f40a968c55a08c4aa39e9166ce354adcc7c76b9
Charles Liu <charles.liu@verkada.com>
2024-08-30 15:40:11 -0700

    add tab for copilot complete

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,37 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      panel = {
+        auto_refresh = true,
+        keymap = {
+          accept = '<CR>',
+          jump_prev = '[[',
+          jump_next = ']]',
+          refresh = 'gr',
+          open = '<M-CR>',
+        },
+      },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = '<M-l>',
+          prev = '<M-[>',
+          next = '<M-]>',
+          dismiss = '<C-]>',
+        },
+      },
+    }
+    vim.keymap.set('i', '<Tab>', function()
+      if require('copilot.suggestion').is_visible() then
+        require('copilot.suggestion').accept()
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
+      end
+    end, {
+      silent = true,
+    })
+  end,
 }
